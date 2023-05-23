3774949aec4fffcc132c40ca095f0c3eca0867e4
Microbob µ <microbob@is88.com>
2023-05-23 00:36:27 -0700

    Attempt to use material design, not orking

--- contents/card.vue
+++ contents/card.vue
@@ -1,10 +1,19 @@
 <template>
-  <div id="goji-card-host">Hello World from the Goji Card</div>
+  <div id="goji-card-host">
+    <p>Hello World from the Goji Card</p>
+    <md-outlined-button>md-outlined-button</md-outlined-button>
+  </div>
 </template>
 
 <script lang="ts">
+import { MdOutlinedButton } from "@material/web/button/outlined-button.js"
 import css from "data-text:~/contents/card.css"
-import type { PlasmoGetStyle } from "plasmo"
+import type { PlasmoCSConfig, PlasmoGetStyle } from "plasmo"
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://www.google.com/*"],
+  all_frames: true
+}
 
 /**
  * Returns a newly created style element with the css content set as its text content
